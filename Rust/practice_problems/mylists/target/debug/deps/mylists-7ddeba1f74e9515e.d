6d22bc74e9f17d593aeb266d425fe0a7ddb72020
Giga Beradze <gbera23@freeuni.edu.ge>
2026-07-28 00:17:46 +0400

    Delete Rust/practice_problems/mylists/target directory

--- Rust/practice_problems/mylists/target/debug/deps/mylists-7ddeba1f74e9515e.d
+++
@@ -1,7 +0,0 @@
-/home/giga/Projects/Rust/mylists/target/debug/deps/mylists-7ddeba1f74e9515e.d: src/main.rs src/mylist.rs src/mylist/linkedlist.rs
-
-/home/giga/Projects/Rust/mylists/target/debug/deps/libmylists-7ddeba1f74e9515e.rmeta: src/main.rs src/mylist.rs src/mylist/linkedlist.rs
-
-src/main.rs:
-src/mylist.rs:
-src/mylist/linkedlist.rs:

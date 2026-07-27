fb864d07c068a1abd438ad89588d96f9446ab847
gbera23-dev <gbera23@freeuni.edu.ge>
2026-07-28 00:13:42 +0400

    added dumb calculator

--- Rust/practice_problems/dumb_calculator/target/debug/deps/dumb_calculator-cba87921fb306b37.d
+++ Rust/practice_problems/dumb_calculator/target/debug/deps/dumb_calculator-cba87921fb306b37.d
@@ -1,7 +1,6 @@
-/home/giga/Projects/Rust/practice_problems/dumb_calculator/target/debug/deps/dumb_calculator-cba87921fb306b37.d: src/main.rs src/dumb_calculator_static.rs src/dumb_calculator_dyn.rs
+/home/giga/Projects/Rust/practice_problems/dumb_calculator/target/debug/deps/dumb_calculator-cba87921fb306b37.d: src/main.rs src/dumb_calculator_static.rs
 
-/home/giga/Projects/Rust/practice_problems/dumb_calculator/target/debug/deps/libdumb_calculator-cba87921fb306b37.rmeta: src/main.rs src/dumb_calculator_static.rs src/dumb_calculator_dyn.rs
+/home/giga/Projects/Rust/practice_problems/dumb_calculator/target/debug/deps/libdumb_calculator-cba87921fb306b37.rmeta: src/main.rs src/dumb_calculator_static.rs
 
 src/main.rs:
 src/dumb_calculator_static.rs:
-src/dumb_calculator_dyn.rs:

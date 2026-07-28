580c8ae48a6e7b326e4f3381b161d3ffe057a962
gbera23-dev <gbera23@freeuni.edu.ge>
2026-07-28 16:52:31 +0400

    added simple json parser(only supports string data type)

--- Rust/practice_problems/json_parser/src/json_parser.rs
+++ Rust/practice_problems/json_parser/src/json_parser.rs
@@ -2,9 +2,15 @@
 use std::collections::HashMap;
 
 const STR_SPLIT: &str = "\"";
-
+const START_JSON: &str = "{";
+const END_JSON: &str = "}"; 
+const START_JSON_LIST: &str = "["; 
+const END_JSON_LIST: &str = "]";
+const KEY_VAL_SEPARATOR: &str = ":";
+ 
+ #[derive(Debug)]
 pub enum JsonVal {
-    SimpleVal(String), 
+    SimpleVal(Option<String>), 
     ComplexVal(Option<HashMap<String, JsonVal>>)
 }
 
@@ -14,18 +20,39 @@ pub fn parse_json(mut json_data:  String) -> Result<HashMap<String, JsonVal>, St
 
     let vec = build_json_token_vec(json_data);
 
-    println!("{:?}", vec); 
+    let mut is_visited = vec![false; vec.len()];
 
-    let mut idx = 0; 
-
-    let map = build_json_map(&vec, &mut idx);
+    let map = build_json_map(&vec, &mut is_visited);
 
     Result::Ok(map)
 }
 
-fn build_json_map(vec: &Vec<String>, idx: &i32) -> HashMap<String, JsonVal> {
+fn build_json_map(vec: &Vec<String>,  is_visited: &mut Vec<bool>) -> HashMap<String, JsonVal> {
 
-    HashMap::<String, JsonVal>::new()
+    let mut map = 
+    HashMap::<String, JsonVal>::new();
+
+    for (idx, str) in vec.iter().enumerate() {
+        if str==START_JSON || is_visited[idx] {continue} 
+
+        is_visited[idx] = true;
+
+        if str==END_JSON{break}
+
+        if str==KEY_VAL_SEPARATOR {
+            let key = vec[idx-1].clone();
+
+            let val = if vec[idx+1] != START_JSON {JsonVal::SimpleVal(Option::Some(vec[idx+1].clone()))}
+
+            else {
+                JsonVal::ComplexVal(Option::Some(build_json_map(&vec, is_visited)))
+            };
+
+            map.insert(key, val); 
+        }
+    }
+
+    map
 }
 
 
@@ -34,8 +61,13 @@ fn clean_json_data(json_data: String) -> String {
     for ch in json_data.bytes().enumerate() {
         if ch.1!=b' ' {
             cleaned_up_data.push(ch.1 as char);
-            if ch.1==b'}' || ch.1==b']'  
-            {cleaned_up_data.push('\"')}
+
+            if ch.1==*END_JSON.as_bytes().get(0).unwrap() || 
+            ch.1==*END_JSON_LIST.as_bytes().get(0).unwrap() ||
+            ch.1==*KEY_VAL_SEPARATOR.as_bytes().get(0).unwrap() {
+            
+                cleaned_up_data.push('\"')
+            }
         };
     } 
     cleaned_up_data
--- Rust/practice_problems/json_parser/src/main.rs
+++ Rust/practice_problems/json_parser/src/main.rs
@@ -1,12 +1,26 @@
 
 mod json_parser; 
 
+use std::fs;
+use std::io; 
+use std::collections::HashMap;
+
+use crate::json_parser::JsonVal; 
+
 fn main() {
-    let _ = json_parser::parse_json(
-        String::from("
-        {
-        \"hello1\" : \"hello2\", 
-        \"jonni\" : {\"a\" : \"b\"} 
-        }
-        ".trim()));
+    let mut file_name = String::new(); 
+    println!("Please, input file name: "); 
+    let _ = io::stdin().read_line(&mut file_name);
+    println!("file name is {}", file_name);
+
+    let res = std::fs::read_to_string(file_name.trim());
+    let map: HashMap<String, JsonVal>;
+
+    if res.is_err() {return}
+
+    map = json_parser::parse_json(res.unwrap()).unwrap();
+        
+    for (size, val) in map.iter().enumerate() {
+        println!("key is {}, val is {:?}", val.0, val.1);
+    }
 }
